5ea43f75c54f0a93b657cc035d77a14e1f24bf43
jinx83424 <ajinkya83424@gmail.com>
2023-07-21 23:46:58 +0530

    Storing in form of Obj

--- sev.js
+++ sev.js
@@ -1,12 +1,17 @@
 function saveToLocal(event){
     event.preventDefault();
-    const name=event.target.name.value;
-    const email=event.target.email.value;
-    const phone=event.target.phone.value;
-    const time=event.target.time.value;
+    const Uname=event.target.name.value;
+    const Uemail=event.target.email.value;
+    const Uphone=event.target.phone.value;
+    const Utime=event.target.time.value;
 
-    localStorage.setItem('name',name)
-    localStorage.setItem('email',email)
-    localStorage.setItem('phone',phone)
-    localStorage.setItem('time',time)
+    let user={
+        name: Uname,
+        email: Uemail,
+        phone: Uphone,
+        time: Utime
+    }
+    
+    userLocal=JSON.stringify(user);
+    localStorage.setItem("UserDetails" ,userLocal)
 }
